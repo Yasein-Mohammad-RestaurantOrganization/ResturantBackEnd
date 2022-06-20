3529e89219a2ff49efc4c7eb6f7aac3b3e64c45a
Yasein Burqan <mr.yasein@gmail.com>
2022-06-20 13:55:10 +0300

    ResturantBackEnd

--- src/model/index.model.js
+++ src/model/index.model.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const drinksModel = require("./cafe.model");
 const contactModel = require("./contact.model");
 const deliveryModel = require("./delivery.model");
+const snacksModel = require("./snacks.model");
 
 const Collection = require("./collection");
 const { Sequelize, DataTypes } = require("sequelize");
@@ -23,17 +24,22 @@ let sequelize = new Sequelize(POSTGRES_URI, sequelizeOptions);
 const drinksTable = drinksModel(sequelize, DataTypes);
 const contactTable = contactModel(sequelize, DataTypes);
 const deliveryTable = deliveryModel(sequelize, DataTypes);
+const snacksTable = snacksModel(sequelize, DataTypes);
 
 const userTable = UserModel(sequelize, DataTypes);
 
 const drinksCollection = new Collection(drinksTable);
 const contactCollection = new Collection(contactTable);
 const deliveryCollection = new Collection(deliveryTable);
+const snacksCollection = new Collection(snacksTable);
 
 module.exports = {
   db: sequelize,
+
   drinks: drinksCollection,
   contact: contactCollection,
   delivery: deliveryCollection,
+  snacks: snacksCollection,
+
   Users: userTable,
 };
